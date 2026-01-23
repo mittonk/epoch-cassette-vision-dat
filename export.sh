a301dc650c38b768f3a94b5389d8afbe1cc1ca6c
Ken Mitton <mittonk@alum.mit.edu>
2026-01-23 07:33:45 -0500

    Date-stamp noi.

--- export.sh
+++ export.sh
@@ -4,4 +4,6 @@
 DESC=`grep description "Epoch Cassette Vision - Games (TNC).dat" | head -1 | sed 's/^.*<description>//' | sed 's/<\/description>.*$//'`
 cp "Epoch Cassette Vision - Games (TNC).dat" "${DESC}.dat"
 
-# TODO: Date-stamp noi version
+# Date-stamp noi version
+VERSION=`grep '<version>' "Epoch - Cassette Vision (noi).dat" | head -1 | sed 's/^.*<version>//' | sed 's/<\/version>.*$//'`
+cp "Epoch - Cassette Vision (noi).dat" "Epoch - Cassette Vision (${VERSION}).dat"
